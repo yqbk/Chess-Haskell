2067cbf9c48685e4a333aaac4d214eb43dd08ca0
Jakub Syrek <jksyrek@gmail.com>
2015-06-13 23:45:14 +0200

    monad play

--- Chess.hs
+++ Chess.hs
@@ -4,10 +4,16 @@ import Board.Moves
 import Board.Board
 import Board.Fields
 import Board.Game
+import Board.Algorithm
 import Board.Pieces
 import Data.List (intercalate)
 import Data.Vector (Vector,toList,fromList,(!),(//))
 import qualified Data.Vector as V
+import System.Environment
+import Control.Monad.State
+import Text.ParserCombinators.Parsec
+import System.IO
+import Data.Maybe
 
 
 type Move = Board->Board
@@ -54,3 +60,68 @@ exampleOpening = [move' "e2" "e4",
 exampleMateGame1 = putStr $ concatMap printPosition $ gameComp mate1
 exampleMateGame2 = putStr $ concatMap printPosition $ gameComp mate2
 start = putStr $ concatMap printPosition $ gameComp (initBoard, White)
+
+-------------------------------------------------------
+
+
+
+
+parseLetter :: Parser Char
+parseLetter = oneOf "abcdefgh"
+
+parseNumber :: Parser Char
+parseNumber = oneOf "12345678"
+
+parsePromotion :: Parser Char
+parsePromotion = oneOf "qrbn"
+
+newtype ACN = ACN (Char,Char,Char,Char,Char)
+
+instance Show ACN where
+  show (ACN (a,b,c,d,y)) = a:b:c:d:y:[]
+
+parseACN :: Parser ACN
+parseACN = do
+          x1 <- parseLetter
+          y1 <- parseNumber
+          x2 <- parseLetter
+          y2 <- parseNumber
+          prm <- option ' ' parsePromotion
+          return $ ACN (x1,y1,x2,y2,prm)
+
+-- stanem jest lista ruchów
+type Gameplay a = StateT ([ACN]) IO a
+
+printHistory :: Show a => [a] -> IO ()
+printHistory h =  do
+  hPutStrLn stderr "Game history"
+  mapM_ (hPutStrLn stderr.show) h
+
+
+play :: String -> Gameplay ()
+play i = do
+  s<- get
+  case parse parseACN "Parsing ACN error" i of
+    Right acn -> (liftIO $ hPutStrLn stderr $ "actual move = " ++ (show acn)) >> put (acn:s)
+    Left _ -> fail ("koniec")
+  liftIO $ printHistory s
+  liftIO $ putStrLn "f3f2" >> hFlush stdout
+
+
+doPlay :: Gameplay ()
+doPlay = liftIO getContents >>= (mapM_ play) . lines
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  --progName <- getProgName
+  --mapM_ putStrLn args
+  --putStrLn progName
+  --hPutStrLn stderr "Chess engine by Zbyszko z Bogdańca ®2015"
+  --let args = ["w"]
+  case (listToMaybe args) of
+    Just "b" -> go
+    Just "w" -> putStrLn "a2c4" >> hFlush stdout>> go -- białe wykonują pierwszy ruch
+    Nothing -> go  -- domyślnie grają czarne
+    where go = evalStateT doPlay []
